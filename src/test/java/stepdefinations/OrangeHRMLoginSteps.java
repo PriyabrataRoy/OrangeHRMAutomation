a59b811d15cbe0e83144435597826e9ebf0d418a
Priyabrata Roy <rpriyabrata11@gmail.com>
2026-06-02 13:22:54 +0530

    Added OrangeHRM Open Url Feature

---
+++ src/test/java/stepdefinations/OrangeHRMLoginSteps.java
@@ -0,0 +1,13 @@
+package stepdefinations;
+
+import base.BaseTest;
+import io.cucumber.java.en.Given;
+
+public class OrangeHRMLoginSteps extends BaseTest {
+
+	@Given("To Launch the browser and navigate the url")
+    public void to_launch_the_browser_and_navigate_the_url() {
+
+        setup();
+    }
+}
---
+++ target/test-classes/feature/01_loginfeature.feature
@@ -0,0 +1,5 @@
+Feature: To test the OrangeHRM Login 
+
+Scenario: To validate Search functionality
+
+Given To Launch the browser and navigate the url
